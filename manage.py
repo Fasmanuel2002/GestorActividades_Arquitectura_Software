cbc694832cfca62c7d44c3a68cab41687f60bef5
ricardoDev1608 <146326523+ricardoDev1608@users.noreply.github.com>
2026-05-08 22:28:11 +0200

    Innitial Commit Ricardo

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+# Ricardo Innitial commit
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "centro_actividades.settings")
